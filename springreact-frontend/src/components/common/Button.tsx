8da13721d59578faa4f31271577f37dfcc60ec05
nilsw13 <nils.wenting@laplateforme.io>
2024-11-26 13:13:23 +0100

    deleted env-variables from properties for mockups credentials

---
+++ springreact-frontend/src/components/common/Button.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import  {Button } from '@/components/ui/button'
+
+function ButtonUI() {
+  return (
+    <Button variant={'outline'} className='border-2 border-red-700'>
+        HELLO
+    </Button>
+  )
+}
+
+export default ButtonUI;
